50b772ae84bf876fe1c3d5f6f8bcfc144c6c3149
Hamza-mtleadmedia <hamza@mtleadmedia.com>
2025-10-28 22:14:19 +0500

    added frontend code

--- frontend-repo/src/utils/apiUtils.js
+++ frontend-repo/src/utils/apiUtils.js
@@ -1,12 +1,14 @@
+
 import axios from 'axios';
 
-
 export const getApiConfig = (token = null, isAdmin = false) => {
-  const headers = {
-    'x-api-key': isAdmin 
-      ? process.env.NEXT_PUBLIC_X_API_ADMIN_KEY 
-      : process.env.NEXT_PUBLIC_X_API_KEY,
-  };
+  const headers = {};
+
+  if (isAdmin) {
+    headers['x-api-admin-key'] = process.env.NEXT_PUBLIC_X_API_ADMIN_KEY;
+  } else {
+    headers['x-api-key'] = process.env.NEXT_PUBLIC_X_API_KEY;
+  }
 
   if (token) {
     headers['Authorization'] = `Bearer ${token}`;
